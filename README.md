df8e402605763d72b20b214766306683bf5f9bcc
Adron Hall <adronhall@gmail.com>
2018-02-27 18:58:18 -0800

    Adding ignore, readme, etc.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Honeycomb Mock Haproxy Style Log Data Writer
+
+Purpose: This repo has code that can be used to write data to Honeycomb.io for testing & experimentation purposes.
+
+How-to: Setup the following with your keys & pertinent information.
+
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/dmgk/faker"
 	"github.com/honeycombio/libhoney-go"
 	"os"
+	"strconv"
 	"time"
 )
 
@@ -13,61 +14,62 @@ func main() {
 
 	libhoney.Init(libhoney.Config{
 		WriteKey: os.Getenv("THRASHER_HONEYCOMBKEY"),
-		Dataset:  "logstash-sample-data-mocker-example",
+		Dataset:  "elastic-comparison",
 	})
 
-	defer libhoney.Close() // Flush any pending calls to Honeycomb
+	defer libhoney.Close()
 
-	go loadHoneycombData(1000000, "load 1")
-	go loadHoneycombData(1000000, "load 2")
-	go loadHoneycombData(1000000, "load 3")
-	go loadHoneycombData(1000000, "load 4")
-	go loadHoneycombData(1000000, "load 5")
+	for i := 1; i < 10; i++ {
+		go loadHoneycombData(100000, "load_"+strconv.Itoa(i))
+	}
 
 	fmt.Print("Working... press key to end.\n")
 	fmt.Scanln()
-	fmt.Println("Finished.\n")
+	fmt.Println("Finished @ " + time.Now().UTC().Format("2006-01-02T15:04:05.999999-07:00"))
 }
 
 func loadHoneycombData(repeat int, ident string) {
 	fmt.Print("started ping " + ident + "\n")
 	for i := 0; i < repeat; i++ {
+		hostLogSource := faker.Internet().IpV4Address()
+		duration, _ := time.ParseDuration(faker.Number().Between(1, 999) + "ms")
+
 		libhoney.SendNow(map[string]interface{}{
 			"time":                      time.Now().UTC().Format("2006-01-02T15:04:05.999999-07:00"),
 			"@version":                  "1",
 			"actconn":                   faker.Number().Between(100, 600),
-			"backend_name":              "645_trapster_" + ident,
+			"backend_name":              faker.Number().Between(100, 125) + "_trapster_" + ident,
 			"backend_queue":             faker.Number().Between(0, 24),
 			"beconn":                    "70",
-			"bytes_read":                144,
+			"bytes_read":                faker.Number().Between(64, 1024),
 			"captured_request_cookie":   "-",
 			"captured_response_cookie":  "-",
 			"client_ip":                 faker.Internet().IpV4Address(),
 			"client_port":               faker.Number().Between(50124, 50127),
 			"facility":                  faker.Number().Between(3, 20),
 			"facility_label":            "local0",
-			"feconn":                    "471",
+			"feconn":                    faker.Number().Between(420, 471),
 			"frontend_name":             "default_" + ident,
 			"host":                      "127.0.0.1",
-			"hostname":                  "ip-10-1-47-121",
-			"http_request":              "/v2/library/ubuntu/manifests/14.04",
+			"hostname":                  hostLogSource,
+			"http_request":              faker.Internet().Url(),
 			"http_status_code":          "500",
 			"http_verb":                 "GET",
 			"http_version":              "1.1",
-			"logsource":                 "ip-10-1-47-121",
-			"pid":                       faker.Number().Between(1111, 8888),
-			"priority":                  134,
+			"logsource":                 hostLogSource,
+			"pid":                       faker.Number().Between(1, 32768),
+			"priority":                  faker.Number().Between(1, 200),
 			"program":                   "haproxy",
-			"request_header_host":       "registry-1.docker.io",
-			"request_header_user_agent": "docker/17.03.2-ce go/go1.7.5 git-commit/f5ec1e2 kernel/4.4.0-75-generic os/linux arch/amd64 UpstreamClient(Docker-Client/17.03.2",
+			"request_header_host":       "registry-x.wherethingsgo.io",
+			"request_header_user_agent": faker.Hacker().Phrases(),
 			"retries":                   faker.Number().Between(1, 3),
-			"server_name":               "\u003cNOSRV\u003e",
+			"server_name":               faker.App().Name(),
 			"service":                   "haproxy",
 			"severity":                  faker.Number().Between(0, 7),
-			"severity_label":            "Informational",
-			"source_host":               "ip-10-1-47-121",
-			"srv_queue":                 "0",
-			"srvconn":                   "0",
+			"severity_label":            faker.Hacker().Verb(),
+			"source_host":               hostLogSource,
+			"srv_queue":                 faker.Number().Between(0, 9),
+			"srvconn":                   faker.Number().Between(0, 999),
 			"tags":                      "[\"haproxy\",\"" + faker.Hacker().Adjective() + "\",\"" + faker.Hacker().Noun() + "\"]",
 			"termination_state":         "PT--",
 			"time_backend_connect":      -1,
@@ -75,7 +77,7 @@ func loadHoneycombData(repeat int, ident string) {
 			"time_duration":             2069,
 			"time_queue":                2070,
 			"time_request":              -1,
-			"timestamp":                 time.Now().UTC().Format("Jan _2 15:04:05"),
+			"timestamp":                 faker.Time().Backward(duration),
 			"type":                      "haproxy",
 		})
 	}
